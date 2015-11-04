4d68b8856052f7e89a1a68fe646e666ddbd546a7
Noura <culurienneldoreth@gmail.com>
2015-11-04 13:00:33 -0800

    thread management

--- wearable/wearable.ino
+++ wearable/wearable.ino
@@ -8,6 +8,13 @@
 // For example if the sampleRate is 20 and N is 100, then we are storing the previous
 // 100 / 20 = 5 seconds worth of data. This is the time scale over which we will compute
 // the average and standard deviation, which are used for spike detection.
+
+// where threads are plugged in
+int threadPin[6] = {3, 5, 6, 7, 8, 9};
+// how much power you think each thread needs. depends on their length etc
+int threadPower[6] = {255, 255, 255, 255, 255, 255};
+// how long (ms) a thread should stay on for before turning off
+#define threadStayOnFor 10000
 ///////////////////////////////
 
 // Low pass bessel filter order=1 alpha1=0.02 samplerate=20
@@ -40,8 +47,15 @@ int sensorFiltered[N];
 
 int samplePeriod = 1000 / sampleRate;
 
+long threadTimeOn[6] = {0, 0, 0, 0, 0, 0};
+bool threadOn[6] = {false, false, false, false, false, false};
+int whichThread = 0;
+
 void setup() {
   Serial.begin(9600);
+  for (int i = 0; i < 6; i++) {
+    pinMode(threadPin[i], OUTPUT);
+  }
 }
 
 void loop() {
@@ -49,14 +63,47 @@ void loop() {
   sensorFilteredNew = myFilter.step(sensorRaw);
   addToArray(sensorFilteredNew);
   
-  //Serial.print(s);Serial.print(" ");Serial.print(sensorIncrease);Serial.println();
-  
   if (hasPeak()) {
     Serial.print("MARK");Serial.print(",");
+    threadOn[whichThread] = true;
+    threadTimeOn[whichThread] = millis();
+    whichThread += 1;
+    whichThread %= 6;
   }
   Serial.print(sensorFilteredNew);Serial.print(",");
+  //Serial.print(sensorRaw);Serial.print(",");
   
+  updateThreads();
+
   delay(samplePeriod);
+}
+
+int threadLastPowered = 0; // this should only be used by updateThreads()
+void updateThreads() {
+  // only turn on one thread each time, then wait for the next time to come around to turn on
+  // a different thread. should keep track of which thread was turned on last time and turn on
+  // a different thread this time
+  
+  analogWrite(threadPin[threadLastPowered], 0);
+  
+  int startThread = (threadLastPowered + 1) % 6;
+  for (int j = 0; j < 6; j++) {
+    int i = (startThread + j + 1) % 6;
+    if (threadOn[i]) {
+      Serial.print("\nthreadPin: ");Serial.print(threadPin[i]);Serial.println();
+      analogWrite(threadPin[i], threadPower[i]);
+      threadLastPowered = i;
+      break;
+    } else {
+      analogWrite(threadPin[i], 0);
+    }
+  }
+  
+  for (int i = 0; i < 6; i++) {
+    if (threadOn[i] && millis() - threadTimeOn[i] > threadStayOnFor) {
+      threadOn[i] = false;
+    }
+  }
 }
 
 void addToArray(int x) {
@@ -87,7 +134,8 @@ bool hasPeak() {
   
   // if the current value is more than a standard deviation above the mean,
   // then that counts as a peak
-  if (sensorFiltered[0] - avg > std) {
+  if (sensorFiltered[0] - avg > std &&
+      sensorFiltered[0] - avg > 25 ) {
     return true;
   } else {
     return false;
